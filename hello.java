9d8099e91df75a294141de1bcaa04562d9f2948c
mldn <408518731@qq.com>
2015-05-22 10:08:25 +0800

    Change hello

--- hello.java
+++ hello.java
@@ -2,6 +2,8 @@ public class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello World!");
 		System.out.println("kuai fang jia ba");
-		
+			System.out.println("kuai fang jia ba");
+				System.out.println("kuai fang jia ba");
+					System.out.println("kuai fang jia ba");
 	}
 }
